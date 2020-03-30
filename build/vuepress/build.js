2ab0fcad935b448c34847ed1e0e1712b957b040f
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-03-30 20:46:13 +0200

    :construction_worker: Add a VuePress build Action
    Co-authored-by: Franck Abgrall <abgrallkefran@gmail.com>

---
+++ build/vuepress/build.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const YAML = require('yaml')
+
+const writeTree = require('./write-tree')
+
+const input = fs.readFileSync('../../defaults.yml', 'utf8')
+const defaults = YAML.parse(input)
+
+writeTree(defaults, 'docs')
