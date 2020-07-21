d887b15bfb3fc98090997a91c105dba89fe3d140
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-07-21 13:51:47 +0200

    :construction_worker: Add a docsify build Action

---
+++ build/docsify/write-tree.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const Handlebars = require('handlebars')
+
+module.exports = (defaults, destinationPath) => {
+  fs.mkdirSync(destinationPath)
+
+  fs.copyFileSync('./templates/index.html', `${destinationPath}/index.html`)
+  fs.copyFileSync('./templates/favicon.ico', `${destinationPath}/favicon.ico`)
+
+  const homeTemplate = fs.readFileSync('./templates/home.md.handlebars', 'utf8')
+  const rootReadmeContent = Handlebars.compile(homeTemplate)(defaults)
+  fs.writeFileSync(`${destinationPath}/readme.md`, rootReadmeContent)
+
+  const sidebarConfigTemplate = fs.readFileSync('./templates/_sidebar.md.handlebars', 'utf8')
+  const sidebarConfigContent = Handlebars.compile(sidebarConfigTemplate)(defaults)
+  fs.writeFileSync(`${destinationPath}/_sidebar.md`, sidebarConfigContent)
+
+  if (defaults.categories !== null) {
+    const categoryTemplate = fs.readFileSync('./templates/category.md.handlebars', 'utf8')
+    const renderCategory = Handlebars.compile(categoryTemplate)
+    defaults.categories.forEach(category => {
+      fs.mkdirSync(`${destinationPath}/${category.folder}`)
+      const categoryReadmeContent = renderCategory(category)
+      fs.writeFileSync(`${destinationPath}/${category.folder}/readme.md`, categoryReadmeContent)
+    })
+
+    const pageTemplate = fs.readFileSync('./templates/page.md.handlebars', 'utf8')
+    const renderPage = Handlebars.compile(pageTemplate)
+    defaults.categories.forEach(({ folder, name, keys }) => {
+      keys.forEach(({ domain, ...page }) => {
+        const pageReadmeContent = renderPage({ ...page, folder, name, domain })
+        fs.writeFileSync(`${destinationPath}/${folder}/${page.key}.md`, pageReadmeContent)
+
+        page.examples.forEach(example => {
+          if (example.image !== undefined) {
+            fs.copyFileSync(
+              `../../images/${folder}/${page.key}/${example.image}`,
+              `${destinationPath}/${folder}/${folder}-${page.key}-${example.image}`
+            )
+          }
+        })
+      })
+    })
+  }
+}
