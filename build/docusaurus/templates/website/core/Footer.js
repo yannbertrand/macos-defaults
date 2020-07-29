4cdd97a83a6c533c4a17652f2f8ce338cbaa84a0
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-07-29 22:33:24 +0200

    :construction_worker: Add a Docusaurus build action

---
+++ build/docusaurus/templates/website/core/Footer.js
@@ -0,0 +1,43 @@
+const React = require('react');
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="nav-footer" id="footer">
+        <section className="sitemap">
+          <a href="/" className="nav-home">
+            {this.props.config.footerIcon && (
+              <img
+                src={this.props.config.baseUrl + this.props.config.footerIcon}
+                alt={this.props.config.title}
+                width="66"
+                height="58"
+              />
+            )}
+          </a>
+          <div>
+            <h5>Docs</h5>
+            <a href="/">
+              Home
+            </a>
+          </div>
+          <div>
+            <h5>More</h5>
+            <a href="https://github.com/yannbertrand/macos-defaults-list">GitHub</a>
+            {this.props.config.twitterUsername && (
+              <div className="social">
+                <a
+                  href={`https://twitter.com/${this.props.config.twitterUsername}`}
+                  className="twitter-follow-button">
+                  Follow @{this.props.config.twitterUsername}
+                </a>
+              </div>
+            )}
+          </div>
+        </section>
+      </footer>
+    );
+  }
+}
+
+module.exports = Footer;
