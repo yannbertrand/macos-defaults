9e258767b2cf16a88c9228b78770758aa8562790
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-08-04 22:35:34 +0200

    :sparkles: Add the screenshot disable-shadow command

---
+++ record/screenshots/disable-shadow/false.js
@@ -0,0 +1,77 @@
+const delay = require('delay')
+const glob = require('glob-promise')
+const robot = require('robotjs')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const { compressPngImage } = require('../../utils')
+
+module.exports = {
+  run: async (outputPath) => {
+    console.log('> Recording screenshot disable-shadow with param set to false')
+
+    const { stderr: setEnvError } = await exec('defaults write com.apple.screencapture show-thumbnail -bool false && defaults write com.apple.screencapture disable-shadow -bool false')
+    if (setEnvError) {
+      console.error('An error occured while setting up the screenshot disble-shadow command')
+      logRollbackInfo()
+      throw new Error(setEnvError)
+    }
+
+    // Preparation
+    robot.moveMouse(robot.getScreenSize().width / 2, robot.getScreenSize().height / 2)
+    robot.mouseClick()
+
+    robot.keyTap('space', 'command')
+    await delay(100)
+    robot.keyTap('s')
+    robot.typeString('afari')
+    await delay(1000)
+    robot.keyTap('enter')
+    await delay(500)
+
+    robot.moveMouse(robot.getScreenSize().width / 2, robot.getScreenSize().height / 2)
+    robot.mouseClick()
+
+    robot.keyTap('l', 'command')
+    await delay(100)
+
+    robot.keyTap('h')
+    robot.typeString('ttps://www.apple.com/macos/')
+    await delay(100)
+    robot.keyTap('enter')
+    await delay(8000)
+
+    // Screenshot
+    robot.keyTap('4', ['command', 'shift'])
+    await delay(100)
+    robot.keyTap('space')
+    await delay(100)
+    robot.keyTap('enter')
+    await delay(100)
+
+    robot.keyTap('w', 'command')
+
+    await delay(1000)
+    const screenshot = (await glob(`/Users/${process.env.USER}/Desktop/*.png`)).pop()
+
+    try {
+      await compressPngImage(screenshot, outputPath, 'false')
+    } catch (compressPngImageError) {
+      logRollbackInfo()
+      throw new Error(compressPngImageError)
+    }
+
+    const { stderr: deleteEnvError } = await exec('defaults delete com.apple.screencapture show-thumbnail && defaults delete com.apple.screencapture disable-shadow')
+    if (deleteEnvError) {
+      console.error('An error occured while cleaning the dock autohide-delay environment')
+      logRollbackInfo()
+      throw new Error(deleteEnvError)
+    }
+
+    return { filepath: `${outputPath}/false`, isVideo: false }
+  }
+}
+
+function logRollbackInfo() {
+  console.info('Please manually run this command to make sure everything is properly reset:')
+  console.info('defaults delete com.apple.screencapture show-thumbnail && defaults delete com.apple.screencapture disable-shadow')
+}
