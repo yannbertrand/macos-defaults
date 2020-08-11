5525cba28cf64a7da8a5843fe4f9513703c4900b
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-08-11 22:22:49 +0200

    :sparkles: Add the dock autohide command

---
+++ record/dock/readme.js
@@ -0,0 +1,23 @@
+const robot = require('robotjs')
+const { captureImage, compressPngImage } = require('../utils')
+
+module.exports = {
+  run: async (outputPath) => {
+    console.log('> Recording dock readme screenshot')
+
+    // Preparation
+    const { width, height } = robot.getScreenSize()
+    const screenshot = `${outputPath}/dock-tmp.png`
+
+    // Screenshot
+    captureImage(0, height - 80, width, 80).write(screenshot)
+
+    try {
+      await compressPngImage(screenshot, outputPath, 'dock')
+    } catch (compressPngImageError) {
+      throw new Error(compressPngImageError)
+    }
+
+    return { filepath: `${outputPath}/dock` }
+  }
+}
