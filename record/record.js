f36d71c0e6617480d8800fd31ef6d2fb6b947e49
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-08-11 21:55:29 +0200

    :camera: Record screenshots and videos programmatically

---
+++ record/record.js
@@ -0,0 +1,33 @@
+const glob = require('glob-promise')
+const path = require('path')
+const robot = require('robotjs')
+
+module.exports = async (files) => {
+  try {
+    const scriptFiles = files.length > 0 ? files : await glob('!(node_modules)/**/*.js')
+
+    console.info(`Found ${scriptFiles.length} scripts to run. Please close Safari and do not move the mouse until the end\n`)
+    robot.keyTap('h', 'command')
+
+    for (const scriptFile of scriptFiles) {
+      const script = require(`./${scriptFile}`)
+      await script.run(getImagePath(scriptFile))
+    }
+
+    console.info(`\nAll videos and screenshots were successfully recorded. You can use your mouse again\n`)
+  } catch (error) {
+    if (error.code === 'RECORDER_TIMEOUT') {
+      console.error(error.message)
+      console.info('The recorder timed out.')
+      console.info('You probably need to activate the screen recording feature for the terminal you\'re using.')
+      console.info('You\'ll find that settings under: System Parameters > Security & Confidentiality > Confidentiality > Screen recording')
+    } else if (error.code === 'ENOTDIR') {
+      console.error(error.message)
+      console.info('A mandatory folder was not found.')
+    } else {
+      console.error('An error occured while recording', error)
+    }
+  }
+}
+
+const getImagePath = file => path.normalize(`../images/${path.dirname(file)}`)
---
+++ record/utils.js
@@ -0,0 +1,102 @@
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const pngquant = require('node-pngquant-native')
+const sharp = require('sharp')
+const Jimp = require('jimp')
+const robot = require('robotjs')
+const { spawn } = require('child_process')
+
+module.exports.captureImage = (x, y, w, h) => {
+  // Taken from https://github.com/octalmage/robotjs/issues/13#issuecomment-501102240
+  const pic = robot.screen.capture(x, y, w, h)
+  const width = pic.byteWidth / pic.bytesPerPixel // pic.width is sometimes wrong!
+  const height = pic.height
+  const image = new Jimp(width, height)
+  let red, green, blue
+  pic.image.forEach((byte, i) => {
+    switch (i % 4) {
+      case 0: return blue = byte
+      case 1: return green = byte
+      case 2: return red = byte
+      case 3:
+        image.bitmap.data[i - 3] = red
+        image.bitmap.data[i - 2] = green
+        image.bitmap.data[i - 1] = blue
+        image.bitmap.data[i] = 255
+    }
+  })
+  return image
+}
+
+module.exports.compressPngImage = async (inputPath, outputFolder, outputName) => {
+  const tmpOutput = `${outputFolder}/${outputName}-tmp.png`
+  const finalOutput = `${outputFolder}/${outputName}.png`
+
+  await mkdirp(outputFolder)
+  fs.renameSync(inputPath, tmpOutput)
+
+  return new Promise((resolve, reject) => {
+    console.info('   Compressing PNG image')
+    sharp(tmpOutput)
+      .resize(740, undefined, { fit: sharp.fit.inside, withoutEnlargement: true })
+      .toFormat('png')
+      .toBuffer()
+      .then(resizedBuffer => {
+        const responseBuffer = pngquant.compress(resizedBuffer)
+        fs.writeFile(finalOutput, responseBuffer, writeFileError => {
+          removeSync(tmpOutput)
+          if (writeFileError) {
+            console.error('An error occured while saving the image')
+            reject(writeFileError)
+          }
+
+          resolve()
+        })
+      })
+  })
+}
+
+module.exports.compressVideo = async (inputPath, outputFolder, outputName) => {
+  const tmpOutput = `${outputFolder}/${outputName}-tmp.mp4`
+  const finalOutput = `${outputFolder}/${outputName}.mp4`
+
+  await mkdirp(outputFolder)
+  fs.renameSync(inputPath, tmpOutput)
+
+  try {
+    await resizeVideo(tmpOutput, finalOutput)
+  } catch (resizeVideoError) {
+    console.error('An error occured while resizing the video')
+    throw new Error(resizeVideoError)
+  } finally {
+    removeSync(tmpOutput)
+  }
+}
+
+function resizeVideo(input, output) {
+  console.info('   Resizing video')
+
+  return new Promise((resolve, reject) => {
+    const ffmpeg = spawn('ffmpeg', ['-i', input, '-vf', 'scale=\'min(740,iw)\':-2', '-c:v', 'libx264', '-crf', '18', '-preset', 'veryslow', '-y', '-c:a', 'copy', output])
+
+    if (process.env.NODE_ENV === 'DEBUG') {
+      ffmpeg.stderr.on('data', function (message) {
+        console.debug(`${message}`)
+      })
+    }
+
+    ffmpeg.on('exit', ffmpegExitCode => {
+      if (ffmpegExitCode === '1') {
+        return reject('ffmpeg')
+      }
+
+      resolve()
+    })
+  })
+}
+
+function removeSync(file) {
+  if (fs.existsSync(file)) {
+    fs.unlinkSync(file)
+  }
+}
