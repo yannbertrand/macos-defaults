2eb6eaf3f054e0d66a18b24031c1bf56ad6f80a1
WorkInProgress-Development <38821590+WorkInProgress-Development@users.noreply.github.com>
2020-10-01 23:17:28 +1000

    Added recording script for menu bar clock

---
+++ record/menubar/timeseparators/off.js
@@ -0,0 +1,55 @@
+const aperture = require('aperture')()
+const delay = require('delay')
+const robot = require('robotjs')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const { makeAppActive, moveAndResizeApp, compressVideo } = require('../../utils')
+
+module.exports = {
+  run: async (outputPath) => {
+    console.log('> Recording menu bar clock FlashDateSeparators with param set to false')
+    // Set the menu bar menuExtras to only show the clock, it will be on the left of notification center, siri, and spotlight search.
+    const { stderr: setEnvError } = await exec(`defaults write com.apple.systemuiserver menuExtras "('/System/Library/CoreServices/Menu Extras/Clock.menu')" && defaults write com.apple.menuextra.clock FlashDateSeparators -bool false && killall SystemUIServer`)
+
+    if (setEnvError) {
+      console.error('An error occured while setting up the menu bar clock FlashDateSeparators command')
+      logRollbackInfo()
+      throw new Error(setEnvError)
+    }
+
+    // Preparation
+    await makeAppActive('Finder')
+    const { width, height } = robot.getScreenSize()
+    const recordWidth = 400
+    const recordHeight = 22
+    const cropArea = {
+      x: width - recordWidth, y: height - recordHeight, // Film the menu bar, which is 22 pixels
+      width: recordWidth, height: recordHeight
+    }
+    // Action!
+    await aperture.startRecording({ cropArea })
+    await delay(2000)
+    const fp = await aperture.stopRecording()
+    // End recording
+    try {
+      await compressVideo(fp, outputPath, 'timeseparators')
+    } catch (compressVideoError) {
+      logRollbackInfo()
+      throw new Error(compressVideoError)
+    }
+
+    const { stderr: deleteEnvError } = await exec('defaults delete com.apple.menuextra.clock FlashDateSeparators && killall SystemUIServer')
+    if (deleteEnvError) {
+      console.error('An error occured while cleaning the menubar clock FlashDateSeparators environment')
+      logRollbackInfo()
+      throw new Error(deleteEnvError)
+    }
+
+    return { filepath: `${outputPath}/timeseparators`, isVideo: true }
+  }
+}
+
+function logRollbackInfo() {
+  console.info('Please manually run this command to make sure everything is properly reset:')
+  console.info('defaults delete com.apple.menuextra.clock FlashDateSeparators && killall SystemUIServer')
+}
