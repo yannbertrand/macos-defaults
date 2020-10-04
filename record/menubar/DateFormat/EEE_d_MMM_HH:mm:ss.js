52dea085f36c97125c0ccaf29bf7359260d4161f
jidicula <johanan.idicula@gmail.com>
2020-10-04 14:14:34 -0400

    [feat][clock] Add recording scripts for clock output options
    **Why this change was necessary**
    It's recommended by the maintainer to have example recordings for each
    command's config options so users know what the expected difference
    will look like.
    
    **What this change does**
    Creates 3 new recording scripts for 3 example options for the
    DateFormat clock output command. These scripts programmatically record
    the UI changes resulting from running the command with a particular
    option.
    
    Note that a new convention for filenames has been introduced - when an
    option has spaces, the corresponding script and recording filename
    replaces the spaces with underscores.
    
    **Any side-effects?**
    Generates new recording mp4 files.
    
    **Additional context/notes/links**
    
    Resolves yannbertrand/macos-defaults#41
    Related to https://github.com/yannbertrand/macos-defaults/pull/56#issuecomment-703285326

---
+++ record/menubar/DateFormat/EEE_d_MMM_HH:mm:ss.js
@@ -0,0 +1,59 @@
+const aperture = require('aperture')()
+const delay = require('delay')
+const robot = require('robotjs')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const { makeAppActive, compressVideo } = require('../../utils')
+
+module.exports = {
+  run: async (outputPath) => {
+    console.log('> Recording menu bar clock DateFormat with param set to "EEE d MMM HH:mm:ss"')
+
+    // Set the menu bar menuExtras to only show the clock, it will be on the left of notification center, siri, and spotlight search.
+    const { stderr: setEnvError } = await exec(`defaults write com.apple.menuextra.clock DateFormat -string "EEE d MMM HH:mm:ss" && killall SystemUIServer && sleep 10`)
+
+    if (setEnvError) {
+      console.error('An error occured while setting up the menu bar clock DateFormat command')
+      logRollbackInfo()
+      throw new Error(setEnvError)
+    }
+
+    // Preparation
+    await makeAppActive('Finder')
+    const { width, height } = robot.getScreenSize()
+    const recordWidth = 400
+    const recordHeight = 22
+    const cropArea = {
+      x: width - recordWidth, y: height - recordHeight, // Film the menu bar, which is 22 pixels
+      width: recordWidth, height: recordHeight
+    }
+
+    // Action!
+    await aperture.startRecording({ cropArea })
+    await delay(2000)
+
+    const fp = await aperture.stopRecording()
+    // End recording
+
+    try {
+      await compressVideo(fp, outputPath, 'EEE_d_MMM_HH:mm:ss')
+    } catch (compressVideoError) {
+      logRollbackInfo()
+      throw new Error(compressVideoError)
+    }
+
+    const { stderr: deleteEnvError } = await exec('defaults delete com.apple.menuextra.clock DateFormat && killall SystemUIServer && sleep 5')
+    if (deleteEnvError) {
+      console.error('An error occured while cleaning the menu bar clock DateFormat environment')
+      logRollbackInfo()
+      throw new Error(deleteEnvError)
+    }
+
+    return { filepath: `${outputPath}/EEE_d_MMM_HH:mm:ss`, isVideo: true }
+  }
+}
+
+function logRollbackInfo() {
+  console.info('Please manually run this command to make sure everything is properly reset:')
+  console.info('defaults delete com.apple.menuextra.clock DateFormat && killall SystemUIServer')
+}
