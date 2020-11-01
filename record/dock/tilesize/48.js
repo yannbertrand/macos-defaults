cfabc1eacc6097c849e91f49a5fddfd6ddf019e9
Moritz Sternemann <moritzsternemann@users.noreply.github.com>
2020-11-01 23:00:26 +0100

    :sparkles: Add dock tilesize command (#85)
    Co-authored-by: Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>

---
+++ record/dock/tilesize/48.js
@@ -0,0 +1,49 @@
+const delay = require('delay')
+const glob = require('glob-promise')
+const robot = require('robotjs')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const { compressPngImage } = require('../../utils')
+
+module.exports = {
+  run: async (outputPath) => {
+    console.log('> Recording dock icon size with param set to 48')
+
+    const { stderr: setEnvError } = await exec('defaults write com.apple.screencapture show-thumbnail -bool false && defaults write com.apple.dock tilesize -int 48 && killall Dock')
+    if (setEnvError) {
+      console.error('An error occured while setting up the dock tilesize command')
+      logRollbackInfo()
+      throw new Error(setEnvError)
+    }
+
+    // Preparation
+    await delay(3000)
+
+    // Screenshot
+    robot.keyTap('3', ['command', 'shift'])
+    
+    await delay(1000)
+    const screenshot = (await glob(`/Users/${process.env.USER}/Desktop/*.png`)).pop()
+
+    try {
+      await compressPngImage(screenshot, outputPath, '48')
+    } catch (compressPngImageError) {
+      logRollbackInfo()
+      throw new Error(compressPngImageError)
+    }
+
+    const { stderr: deleteEnvError } = await exec('defaults delete com.apple.screencapture show-thumbnail && defaults delete com.apple.dock tilesize && killall Dock')
+    if (deleteEnvError) {
+      console.error('An error occured while cleaning the dock tilesize environment')
+      logRollbackInfo()
+      throw new Error(deleteEnvError)
+    }
+
+    return { filepath: `${outputPath}/48` }
+  }
+}
+
+function logRollbackInfo() {
+  console.info('Please manually run this command to make sure everything is properly reset:')
+  console.info('defaults delete com.apple.screencapture show-thumbnail && defaults delete com.apple.dock tilesize && killall Dock')
+}
