ea8a2f233a2108b144fd0378d2e00f330884042b
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-11-01 10:36:07 +0100

    :camera: Copy the working script

--- record/finder/NSTableViewDefaultSizeMode/3.js
+++ record/finder/NSTableViewDefaultSizeMode/3.js
@@ -1,88 +1,71 @@
-const aperture = require("aperture")();
-const delay = require("delay");
-const robot = require("robotjs");
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
-const {
-  makeAppActive,
-  moveAndResizeApp,
-  compressPngImage,
-} = require("../../utils");
+const delay = require('delay')
+const robot = require('robotjs')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+const { makeAppActive, moveAndResizeApp, captureImage, compressPngImage } = require('../../utils')
 
 module.exports = {
   run: async (outputPath) => {
-    console.log(
-      "> Recording finder NSTableViewDefaultSizeMode with param set to 3"
-    );
+    console.log('> Recording finder NSTableViewDefaultSizeMode with param set to 3')
 
-    const { stderr: setEnvError } = await exec(
-      "defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 3 && killall Finder"
-    );
+    const { stderr: setEnvError } = await exec('defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 3 && killall Finder')
     if (setEnvError) {
-      console.error(
-        "An error occured while setting up the finder NSTableViewDefaultSizeMode command"
-      );
-      logRollbackInfo();
-      throw new Error(setEnvError);
+      console.error('An error occured while setting up the finder NSTableViewDefaultSizeMode command')
+      logRollbackInfo()
+      throw new Error(setEnvError)
     }
-    await delay(1000);
+    await delay(1000)
 
     // Preparation
-    await makeAppActive("Finder");
-    await delay(500);
+    await makeAppActive('Finder')
+    robot.keyTap('g', ['command', 'shift'])
+    await delay(100)
+    robot.keyTap('right')
+    robot.keyTap('a', 'command')
+    robot.keyTap('backspace')
+    robot.typeString('~/macos-defaults/')
+    await delay(1000)
+    robot.keyTap('enter')
+    await delay(500)
 
-    const { width, height } = robot.getScreenSize();
-    const recordWidth = 720;
-    const recordHeight = 404;
+    const { width, height } = robot.getScreenSize()
+    const recordWidth = 720
+    const recordHeight = 404
     const cropArea = {
-      x: width / 2 - recordWidth / 2,
-      y: 345,
-      width: recordWidth,
-      height: recordHeight,
-    };
-    const pos1 = { x: cropArea.x + recordWidth / 3, y: cropArea.y };
-    const pos2 = {
-      x: cropArea.x + recordWidth / 3,
-      y: cropArea.y - recordHeight / 2 + 38,
-    };
+      x: width / 2 - recordWidth / 2, y: 345,
+      width: recordWidth, height: recordHeight
+    }
 
-    await moveAndResizeApp("Finder", cropArea, height);
+    await moveAndResizeApp('Finder', cropArea, height)
+    await delay(1000)
+    const screenshot = `${outputPath}/3.png`
 
     // Screenshot
-    robot.keyTap("3", ["command", "shift"]);
-    await delay(1000);
-    const screenshot = (
-      await glob(`/Users/${process.env.USER}/Desktop/*.png`)
-    ).pop();
+    captureImage(
+      cropArea.x, height - recordHeight - cropArea.y,
+      cropArea.width, cropArea.height
+    ).write(screenshot)
 
     try {
-      await compressPngImage(screenshot, outputPath, "3");
+      await compressPngImage(screenshot, outputPath, '3')
     } catch (compressPngImageError) {
-      logRollbackInfo();
-      throw new Error(compressPngImageError);
+      logRollbackInfo()
+      throw new Error(compressPngImageError)
     }
 
-    const { stderr: deleteEnvError } = await exec(
-      "defaults delete NSGlobalDomain NSTableViewDefaultSizeMode && killall Finder"
-    );
+    const { stderr: deleteEnvError } = await exec('defaults delete NSGlobalDomain NSTableViewDefaultSizeMode && killall Finder')
     if (deleteEnvError) {
-      console.error(
-        "An error occured while cleaning the finder NSTableViewDefaultSizeMode environment"
-      );
-      logRollbackInfo();
-      throw new Error(deleteEnvError);
+      console.error('An error occured while cleaning the finder NSTableViewDefaultSizeMode environment')
+      logRollbackInfo()
+      throw new Error(deleteEnvError)
     }
-    await delay(1000);
+    await delay(1000)
 
-    return { filepath: `${outputPath}/3`, isVideo: true };
+    return { filepath: `${outputPath}/3` }
   },
-};
+}
 
 function logRollbackInfo() {
-  console.info(
-    "Please manually run this command to make sure everything is properly reset:"
-  );
-  console.info(
-    "defaults delete NSGlobalDomain NSTableViewDefaultSizeMode && killall Finder"
-  );
+  console.info('Please manually run this command to make sure everything is properly reset:')
+  console.info('defaults delete NSGlobalDomain NSTableViewDefaultSizeMode && killall Finder')
 }
