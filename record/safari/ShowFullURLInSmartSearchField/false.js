1a01f7192f5ddcc1e03d51dac561804f60854338
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2022-08-19 11:48:58 +0200

    :sparkles: Add screenshots for Safari full URL display command

---
+++ record/safari/ShowFullURLInSmartSearchField/false.js
@@ -0,0 +1,42 @@
+const MacRunner = require('../../mac-runner')
+const { compressPngImage } = require('../../utils')
+
+module.exports = {
+  run: async (outputPath) => {
+    console.log(
+      '> Recording safari ShowFullURLInSmartSearchField with param set to false'
+    )
+
+    try {
+      const runner = new MacRunner()
+      await runner
+        .setDefault('com.apple.Safari', 'ShowFullURLInSmartSearchField', '-bool false', '0')
+        .openApp('Safari', '-F https://www.apple.com/macos/monterey/')
+        .activateApp('Safari')
+        .moveAndResizeApp('Safari', 0, 0, 740, 250)
+        .captureApp('Safari', `${outputPath}/false-tmp.png`)
+        .deleteDefault('com.apple.Safari', 'ShowFullURLInSmartSearchField')
+        .killApp('Safari')
+        .run()
+    } catch (runnerError) {
+      logRollbackInfo()
+      throw new Error(runnerError)
+    }
+
+    try {
+      await compressPngImage(`${outputPath}/false-tmp.png`, outputPath, 'false')
+    } catch (compressPngImageError) {
+      logRollbackInfo()
+      throw new Error(compressPngImageError)
+    }
+
+    return { filepath: `${outputPath}/false` }
+  },
+}
+
+function logRollbackInfo() {
+  console.info(
+    'Please manually run this command to make sure everything is properly reset:'
+  )
+  console.info('defaults delete com.apple.Safari ShowFullURLInSmartSearchField && killall Safari')
+}
